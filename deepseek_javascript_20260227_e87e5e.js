9aa5ec49d4c574096c89a8e18bec264fb8114448
filmiduniyamix <filmiduniyamix@gmail.com>
2026-02-27 22:09:21 +0530

    Delete deepseek_javascript_20260227_e87e5e.js

--- deepseek_javascript_20260227_e87e5e.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { resolveUrl } = require('../controllers/resolveController');
-const router = express.Router();
-
-router.post('/resolve', resolveUrl);
-
-module.exports = router;
