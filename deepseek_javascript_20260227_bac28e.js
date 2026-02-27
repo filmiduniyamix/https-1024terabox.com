21703de480f4062aa96bce1ee82b510685c0b012
filmiduniyamix <filmiduniyamix@gmail.com>
2026-02-27 22:08:55 +0530

    Delete deepseek_javascript_20260227_bac28e.js

--- deepseek_javascript_20260227_bac28e.js
+++
@@ -1,6 +0,0 @@
-const logger = {
-  info: (msg) => console.log(`[INFO] ${new Date().toISOString()} - ${msg}`),
-  error: (msg) => console.error(`[ERROR] ${new Date().toISOString()} - ${msg}`)
-};
-
-module.exports = { logger };
